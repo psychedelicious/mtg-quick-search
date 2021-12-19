a256180e0b10732e20dd9a0478c940fa8bc87f07
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2021-12-19 12:30:54 +1100

    Updates app icons

--- bin/build-chrome.sh
+++ bin/build-chrome.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # copy icons
-cp assets/icon16.png dist/icon16.png
-cp assets/icon48.png dist/icon48.png
-cp assets/icon128.png dist/icon128.png
+cp assets/icon16x16.png dist/icon16x16.png
+cp assets/icon48x48.png dist/icon48x48.png
+cp assets/icon128x128_padding.png dist/icon128x128_padding.png
 
 # copy manifest
 cp src/manifestChrome.json dist/manifest.json
--- bin/build-ff.sh
+++ bin/build-ff.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # copy icons
-cp assets/icon16.png dist/icon16.png
-cp assets/icon48.png dist/icon48.png
-cp assets/icon.svg dist/icon.svg
+cp assets/icon16x16.png dist/icon16x16.png
+cp assets/icon48x48.png dist/icon48x48.png
+cp assets/icon96x96.png dist/icon96x96.png
 
 # copy manifest
 cp src/manifestFirefox.json dist/manifest.json
